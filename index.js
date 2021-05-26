75b6e295d7e3d3adcfad19462e9f38c20f32fb29
Felipe Ribeiro <eusouocara3000@gmail.com>
2021-05-26 08:53:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 let countHtml = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 let count = 0;
 
-function increment() {
-    count++
-    countHtml.innerText = count
+function plus() {
+    count+=1;
+    countHtml.textContent = count;
+}
+
+function minus() {
+    count-=1;
+    countHtml.textContent = count;
+}
+
+function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countHtml.textContent = 0;
+    count = 0;
 }
